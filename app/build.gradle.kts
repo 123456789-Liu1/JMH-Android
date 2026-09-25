3032e7c5e82157ddc3f74c0c6e7aba57d0223d2b
PRCS <259987992+123456789-Liu1@users.noreply.github.com>
2026-09-25 21:16:23 +0800

    chore(release): v2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.jmh.app"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
